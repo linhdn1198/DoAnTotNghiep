7deea5a6c624ca7974a9ec73b7afaa6c14f705f2
Linh Duong <linhdn1198@gmail.com>
2019-12-02 22:22:43 +0700

    feat(Frontend): Load data page index

--- resources/views/clients/modules/header.blade.php
+++ resources/views/clients/modules/header.blade.php
@@ -57,8 +57,8 @@
     </div>
     <div class="search_input" id="search_input_box">
         <div class="container ">
-            <form class="d-flex justify-content-between search-inner">
-                <input type="text" class="form-control" id="search_input" placeholder="Search Here">
+            <form class="d-flex justify-content-between search-inner" action="{{ route('search_product') }}" method="GET">
+                <input type="text" class="form-control" id="search" name="search" placeholder="{{ __('home.search') }}">
                 <button type="submit" class="btn"></button>
                 <span class="ti-close" id="close_search" title="Close Search"></span>
             </form>
