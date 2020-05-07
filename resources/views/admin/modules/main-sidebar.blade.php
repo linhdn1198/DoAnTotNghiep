c9694a703914fcf2e3378e9740410989f350c4c9
Linh Duong <linhdn1198@gmail.com>
2020-05-07 22:26:45 +0700

    Update avatart

--- resources/views/admin/modules/main-sidebar.blade.php
+++ resources/views/admin/modules/main-sidebar.blade.php
@@ -10,7 +10,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="/admin/linhdn1198.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="{{ route('users.edit', Auth::user()->id ) }}" class="d-block">{{ Auth::user()->name }}</a>
